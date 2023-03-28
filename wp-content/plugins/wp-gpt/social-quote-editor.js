8ede6889257eb4a55d18ee1469d1e7b89322668d
Paolo Belcastro <p3ob7o@users.noreply.github.com>
2023-03-29 01:18:35 +0200

    Fixed social-quote.js

--- wp-content/plugins/wp-gpt/social-quote-editor.js
+++ wp-content/plugins/wp-gpt/social-quote-editor.js
@@ -1,16 +1,19 @@
 const { registerBlockType } = wp.blocks;
 const { __ } = wp.i18n;
+const { Fragment } = wp.element;
 
 registerBlockType('wp-gpt/social-quote', {
     title: __('Social Quote', 'wp-gpt'),
     icon: 'twitter',
     category: 'common',
-    edit: () => {
-        // Add your edit block code here
-        return <div>{__('Social Quote Block', 'wp-gpt')}</div>;
+    edit: (props) => {
+        return (
+            <Fragment>
+                <div>{__('Social Quote Block', 'wp-gpt')}</div>
+            </Fragment>
+        );
     },
     save: () => {
-        // Add your save block code here
         return null;
     },
 });
