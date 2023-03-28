a1606b7258f5dfb6ca03e8357f8bc8e8c6b9d2a0
Paolo Belcastro <p3ob7o@users.noreply.github.com>
2023-03-29 00:50:43 +0200

    Adding console.log() statements for debugging

--- wp-content/plugins/wp-gpt/gpt-settings.js
+++ wp-content/plugins/wp-gpt/gpt-settings.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOMContentLoaded'); // Add this line
     const saveButton = document.querySelector('#gpt_save_api_key');
+    console.log('saveButton:', saveButton); // Add this line
 
     if (saveButton) {
         saveButton.addEventListener('click', () => {
+            console.log('Button clicked'); // Add this line
             const apiKey = document.querySelector('#gpt_api_key').value;
             const data = { gpt_api_key: apiKey };
 
@@ -14,8 +17,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 },
                 body: JSON.stringify(data),
             })
-                .then((response) => response.json())
+                .then((response) => {
+                    console.log('Response:', response); // Add this line
+                    return response.json();
+                })
                 .then((data) => {
+                    console.log('Data:', data); // Add this line
                     if (data.status === 'success') {
                         alert('API key saved successfully.');
                     } else {
