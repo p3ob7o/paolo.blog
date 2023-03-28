82a81224ef2454d77671addac0bbe55e7ccba8f3
Paolo Belcastro <p3ob7o@users.noreply.github.com>
2023-03-28 18:27:34 +0200

    Modified PHP to list all images in the post and queue JS code

--- wp-content/plugins/jetpack-carousel-custom/jetpack-carousel-custom.php
+++ wp-content/plugins/jetpack-carousel-custom/jetpack-carousel-custom.php
@@ -8,9 +8,39 @@ Author: Paolo Belcastro
 Author URI: https://paolo.blog
 */
 
-add_filter( 'jetpack_carousel_options', 'my_jetpack_carousel_options' );
+add_filter( 'the_content', 'my_add_carousel_links' );
 
-function my_jetpack_carousel_options( $options ) {
-    $options['single_image_nav'] = true;
-    return $options;
+function my_add_carousel_links( $content ) {
+    // Find all image tags in the content
+    preg_match_all('/<img[^>]+>/i', $content, $matches);
+    $images = $matches[0];
+
+    // Create an array of image URLs
+    $urls = array();
+    foreach ( $images as $image ) {
+        preg_match('/src="([^"]*)"/i', $image, $url);
+        $urls[] = $url[1];
+    }
+
+    // If there are no images, return the original content
+    if ( empty( $urls ) ) {
+        return $content;
+    }
+
+    // Add links to open images in a Jetpack carousel
+    $links = array();
+    foreach ( $urls as $url ) {
+        $links[] = '<a href="' . $url . '" class="my-jetpack-carousel-link"></a>';
+    }
+    $content .= implode( '', $links );
+
+    return $content;
+}
+
+add_action( 'wp_enqueue_scripts', 'my_enqueue_scripts' );
+
+function my_enqueue_scripts() {
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'jetpack-carousel', 'https://s1.wp.com/wp-content/mu-plugins/jetpack/_inc/build/carousel/jetpack-carousel.min.js', array( 'jquery' ), JETPACK__VERSION );
+    wp_enqueue_script( 'my-jetpack-carousel', plugin_dir_url( __FILE__ ) . 'my-jetpack-carousel.js', array( 'jquery', 'jetpack-carousel' ), '1.0', true );
 }
