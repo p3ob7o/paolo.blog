2c859558d980183447d439093ace5c9ecd606a4d
Paolo Belcastro <p3ob7o@users.noreply.github.com>
2023-03-28 18:40:29 +0200

    New iteration, closer to how the carousel actually works

--- wp-content/plugins/jetpack-carousel-custom/jetpack-carousel-custom.php
+++ wp-content/plugins/jetpack-carousel-custom/jetpack-carousel-custom.php
@@ -8,39 +8,58 @@ Author: Paolo Belcastro
 Author URI: https://paolo.blog
 */
 
-add_filter( 'the_content', 'my_add_carousel_links' );
+// Add custom data to the Jetpack Carousel script
+add_filter( 'jetpack_carousel_data', 'my_jetpack_carousel_data' );
 
-function my_add_carousel_links( $content ) {
-    // Find all image tags in the content
-    preg_match_all('/<img[^>]+>/i', $content, $matches);
-    $images = $matches[0];
+function my_jetpack_carousel_data( $data ) {
+    // Find all images on the page
+    $images = get_posts( array(
+        'post_type'      => 'attachment',
+        'posts_per_page' => -1,
+        'post_mime_type' => 'image',
+        'post_status'    => 'inherit',
+        'fields'         => 'ids',
+    ) );
 
-    // Create an array of image URLs
-    $urls = array();
+    // Create an array of gallery data for all images on the page
+    $gallery_data = array();
     foreach ( $images as $image ) {
-        preg_match('/src="([^"]*)"/i', $image, $url);
-        $urls[] = $url[1];
+        $gallery_data[] = array(
+            'src'       => wp_get_attachment_url( $image ),
+            'title'     => get_the_title( $image ),
+            'caption'   => get_post_field( 'post_excerpt', $image ),
+            'mime_type' => get_post_mime_type( $image ),
+        );
     }
 
-    // If there are no images, return the original content
-    if ( empty( $urls ) ) {
-        return $content;
-    }
+    // Add custom data to the Jetpack Carousel script
+    $data['gallery_data'] = $gallery_data;
 
-    // Add links to open images in a Jetpack carousel
-    $links = array();
-    foreach ( $urls as $url ) {
-        $links[] = '<a href="' . $url . '" class="my-jetpack-carousel-link"></a>';
-    }
-    $content .= implode( '', $links );
-
-    return $content;
+    return $data;
 }
 
-add_action( 'wp_enqueue_scripts', 'my_enqueue_scripts' );
+// Modify the Jetpack Carousel script to create a custom gallery
+add_action( 'wp_enqueue_scripts', 'my_enqueue_jetpack_carousel' );
 
-function my_enqueue_scripts() {
-    wp_enqueue_script( 'jquery' );
-    wp_enqueue_script( 'jetpack-carousel', 'https://s1.wp.com/wp-content/mu-plugins/jetpack/_inc/build/carousel/jetpack-carousel.min.js', array( 'jquery' ), JETPACK__VERSION );
-    wp_enqueue_script( 'my-jetpack-carousel', plugin_dir_url( __FILE__ ) . 'my-jetpack-carousel.js', array( 'jquery', 'jetpack-carousel' ), '1.0', true );
+function my_enqueue_jetpack_carousel() {
+    // Enqueue the Jetpack Carousel script
+    wp_enqueue_script( 'jetpack-carousel' );
+
+    // Add a custom script that modifies the Jetpack Carousel behavior
+    wp_enqueue_script( 'my-jetpack-carousel', plugin_dir_url( __FILE__ ) . 'my-jetpack-carousel.js', array( 'jetpack-carousel' ), '1.0', true );
 }
+
+// Add a click event listener to all images on the page that opens the custom gallery
+add_action( 'wp_footer', 'my_add_gallery_links' );
+
+function my_add_gallery_links() {
+    // Find all images on the page
+    $images = get_posts( array(
+        'post_type'      => 'attachment',
+        'posts_per_page' => -1,
+        'post_mime_type' => 'image',
+        'post_status'    => 'inherit',
+        'fields'         => 'ids',
+    ) );
+
+    // Add a click event listener to
