02635db2c33979cf221d4e8fe138702f4e47d8dc
Paolo Belcastro <p3ob7o@users.noreply.github.com>
2023-03-28 18:43:13 +0200

    Updated JS to match PHP

--- wp-content/plugins/jetpack-carousel-custom/jetpack-carousel.js
+++ wp-content/plugins/jetpack-carousel-custom/jetpack-carousel.js
@@ -1,15 +1,26 @@
 jQuery(document).ready(function($) {
-    // Add click event listener to carousel links
-    $('a.my-jetpack-carousel-link').on('click', function(e) {
+    // Add click event listener to gallery links
+    $('a.my-gallery-link').on('click', function(e) {
         e.preventDefault();
 
-        // Find all carousel links
-        var links = $('a.my-jetpack-carousel-link');
+        // Find all gallery links
+        var links = $('a.my-gallery-link');
 
-        // Find the index of the clicked link
-        var index = links.index(this);
+        // Create an array of gallery data from the links
+        var galleryData = [];
+        links.each(function() {
+            galleryData.push({
+                src: $(this).attr('href'),
+                title: '',
+                caption: '',
+                mime_type: 'image'
+            });
+        });
 
-        // Open the Jetpack carousel with all images on the page
-        $(document).trigger('click', { index: index, gallery: links.toArray() });
+        // Open the custom Jetpack Carousel gallery
+        $(document).trigger('click', {
+            gallery: galleryData,
+            start: links.index(this)
+        });
     });
 });
