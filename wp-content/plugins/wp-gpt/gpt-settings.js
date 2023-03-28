f7482a0db2513f19470fec0d95c9fed472865053
Paolo Belcastro <p3ob7o@users.noreply.github.com>
2023-03-29 00:57:20 +0200

    Removed console.log() statements

--- wp-content/plugins/wp-gpt/gpt-settings.js
+++ wp-content/plugins/wp-gpt/gpt-settings.js
@@ -1,11 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded'); // Add this line
     const saveButton = document.querySelector('#gpt_save_api_key');
-    console.log('saveButton:', saveButton); // Add this line
 
     if (saveButton) {
         saveButton.addEventListener('click', () => {
-            console.log('Button clicked'); // Add this line
             const apiKey = document.querySelector('#gpt_api_key').value;
             const data = { gpt_api_key: apiKey };
 
@@ -18,11 +15,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 body: JSON.stringify(data),
             })
                 .then((response) => {
-                    console.log('Response:', response); // Add this line
                     return response.json();
                 })
                 .then((data) => {
-                    console.log('Data:', data); // Add this line
                     if (data.status === 'success') {
                         alert('API key saved successfully.');
                     } else {
