ec51b7e14bd7503279ec3b7e034c80949d150cc8
Paolo Belcastro <p3ob7o@users.noreply.github.com>
2023-03-29 12:01:39 +0200

    Remove JSX syntax

--- wp-content/plugins/wp-gpt/social-quote-editor.js
+++ wp-content/plugins/wp-gpt/social-quote-editor.js
@@ -1,19 +1,22 @@
 const { registerBlockType } = wp.blocks;
 const { __ } = wp.i18n;
-const { Fragment } = wp.element;
+const { InnerBlocks, useBlockProps } = wp.blockEditor;
+const { createElement } = wp.element;
 
 registerBlockType('wp-gpt/social-quote', {
     title: __('Social Quote', 'wp-gpt'),
+    description: __('A block that generates a shareable quote from the post content.', 'wp-gpt'),
+    category: 'widgets',
     icon: 'twitter',
-    category: 'common',
-    edit: (props) => {
-        return (
-            <Fragment>
-                <div>{__('Social Quote Block', 'wp-gpt')}</div>
-            </Fragment>
-        );
+    supports: {
+        html: false,
     },
-    save: () => {
-        return null;
+    edit() {
+        const blockProps = useBlockProps();
+        return createElement('div', blockProps, createElement(InnerBlocks));
+    },
+    save() {
+        const blockProps = useBlockProps.save();
+        return createElement('div', blockProps, createElement(InnerBlocks.Content));
     },
 });
