aadace282faaee17ef1e102a3a586a651295bd1d
Paolo Belcastro <p3ob7o@users.noreply.github.com>
2023-03-30 01:00:17 +0200

    Fixed apiFetch import

--- wp-content/plugins/wp-gpt/social-quote-editor.js
+++ wp-content/plugins/wp-gpt/social-quote-editor.js
@@ -1,11 +1,10 @@
 const { registerBlockType } = wp.blocks;
 const { __ } = wp.i18n;
 const { InnerBlocks, useBlockProps } = wp.blockEditor;
-const { createElement, useEffect } = wp.element;
-const { useState } = wp.react;
+const { createElement, useState, useEffect } = wp.element;
 const { TextControl, Button } = wp.components;
 const { useSelect } = wp.data;
-const { apiFetch } = wp;
+const { apiFetch } = wp.apiFetch;
 
 
 
