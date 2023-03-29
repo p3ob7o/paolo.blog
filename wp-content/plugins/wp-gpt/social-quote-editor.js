9bdc71374dfd33671e9a30c8e372900fc6fe504d
Paolo Belcastro <p3ob7o@users.noreply.github.com>
2023-03-30 00:36:27 +0200

    Implemented edit() function

--- wp-content/plugins/wp-gpt/social-quote-editor.js
+++ wp-content/plugins/wp-gpt/social-quote-editor.js
@@ -12,9 +12,67 @@ registerBlockType('wp-gpt/social-quote', {
         html: false,
     },
     edit() {
-        const blockProps = useBlockProps();
-        return createElement('div', blockProps, createElement(InnerBlocks));
-    },
+    const [quote, setQuote] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const blockProps = useBlockProps();
+
+    const content = useSelect((select) => {
+        const blockEditor = select('core/block-editor');
+        if (!blockEditor) {
+            return '';
+        }
+        const blocks = blockEditor.getBlocks();
+        const contentBlocks = blocks.slice(0, -1); // Exclude the last block (our Social Quote block)
+        return contentBlocks.map((block) => block.originalContent).join('\n');
+    }, []);
+
+    useEffect(() => {
+        if (!content) {
+            return;
+        }
+
+        setLoading(true);
+
+        apiFetch({
+            path: '/wp-gpt/proxy.php',
+            method: 'POST',
+            data: {
+                prompt: "As a social content expert creator, extract the best quote from the provided text. 'Best quote' means the one more likely to drive readers to want to read the content if they were to see the quote on Twitter. Limit the quote to 200 characters maximum",
+                context: content,
+            },
+        })
+            .then((data) => {
+                setLoading(false);
+                setQuote(data.choices[0].text.trim());
+            })
+            .catch(() => {
+                setLoading(false);
+            });
+    }, [content]);
+
+    function tweetQuote() {
+        if (!quote) {
+            return;
+        }
+
+        const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(quote)}`;
+        window.open(tweetUrl, '_blank');
+    }
+
+    return createElement(
+        'div',
+        blockProps,
+        loading ? (
+            createElement(Spinner)
+        ) : (
+            createElement('div', { className: 'wp-gpt-social-quote' }, [
+                createElement('p', {}, quote),
+                createElement(Button, { isSecondary: true, onClick: tweetQuote }, 'Tweet this'),
+            ])
+        )
+    );
+},
     save() {
         const blockProps = useBlockProps.save();
         return createElement('div', blockProps, createElement(InnerBlocks.Content));
