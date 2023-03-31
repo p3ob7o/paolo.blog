8ba236f8da2834ed366ab23788489866cf78895a
Paolo Belcastro <p3ob7o@users.noreply.github.com>
2023-03-31 23:50:05 +0200

    Update social-quote-editor.js

--- wp-content/plugins/wp-gpt/social-quote-editor.js
+++ wp-content/plugins/wp-gpt/social-quote-editor.js
@@ -1,90 +1,114 @@
 const { registerBlockType } = wp.blocks;
+const { __ } = wp.i18n;
+const { InnerBlocks, useBlockProps } = wp.blockEditor;
+const { createElement, useState, useEffect } = wp.element;
+const { TextControl, Button, Spinner } = wp.components;
 const { useSelect } = wp.data;
-const { useState, useEffect } = wp.element;
-const { RichText } = wp.editor;
-const { Button } = wp.components;
+const apiFetch = wp.apiFetch || wp.api.apiFetch;
+const { addQueryArgs } = wp.url;
+
+
+const EditSocialQuote = (props) => {
+  try {
+    const [quote, setQuote] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const blockProps = useBlockProps();
+
+    const content = useSelect((select) => {
+      const blockEditor = select('core/block-editor');
+      if (!blockEditor) {
+        return '';
+      }
+      const blocks = blockEditor.getBlocks();
+      const contentBlocks = blocks.slice(0, -1); // Exclude the last block (our Social Quote block)
+      return contentBlocks.map((block) => block.originalContent).join('\n');
+    }, []);
+
+	useEffect(() => {
+	  if (!content) {
+		return;
+	  }
+
+	  setLoading(true);
+
+	  const path = addQueryArgs('/wp-gpt/proxy.php', {});
+	  console.log('Generated path:', path);
+
+	  fetch('/wp-content/plugins/wp-gpt/proxy.php', {
+		method: 'POST',
+		headers: {
+		  'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+		  prompt: "As a social content expert creator, extract the best quote from the provided text. 'Best quote' means the one more likely to drive readers to want to read the content if they were to see the quote on Twitter. Limit the quote to 200 characters maximum, and avoid using hashtags",
+		  context: content,
+		}),
+	  })
+		.then((response) => response.json())
+	.then((data) => {
+	  console.log('Data received:', data);
+	  if (!data.choices || data.choices.length === 0) {
+		console.error('No choices found in the response data');
+		setLoading(false);
+		return;
+	  }
+	  setLoading(false);
+		const assistantMessage = data.choices[0].message.content;
+		setQuote(assistantMessage.trim());
+		})
+		.catch((error) => {
+		  console.error('Fetch error:', error);
+		  setLoading(false);
+		});
+	}, [content]);
+
+	async function tweetQuote() {
+	  if (!quote) {
+		return;
+	  }
+
+	  const postId = wp.data.select('core/editor').getCurrentPostId();
+	  const response = await fetch(`/wp-json/wp/v2/posts/${postId}`);
+	  const post = await response.json();
+	  const permalink = post.link;
+
+	  const tweetContent = `${quote}\n${permalink}`;
+	  const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetContent)}`;
+	  window.open(tweetUrl, '_blank');
+	}
+	
+    return createElement(
+      'div',
+      blockProps,
+      loading ? (
+        createElement(Spinner)
+      ) : (
+        createElement('div', { className: 'wp-gpt-social-quote' }, [
+          createElement('p', {}, quote),
+          createElement(Button, { isSecondary: true, onClick: tweetQuote }, 'Tweet this'),
+        ])
+      )
+    );
+  } catch (error) {
+    console.error('Error in Social Quote block:', error);
+  }
+};
 
 registerBlockType('wp-gpt/social-quote', {
-    title: 'Social Quote',
-    icon: 'twitter',
-    category: 'common',
-    attributes: {
-        quote: {
-            type: 'string',
-            default: '',
-        },
-    },
-
-    edit: (props) => {
-        const { attributes, setAttributes } = props;
-        const [isLoading, setIsLoading] = useState(false);
-
-        const contentBefore = useSelect((select) => {
-            const blocks = select('core/block-editor').getBlocks();
-            const currentBlockIndex = blocks.findIndex((block) => block.clientId === props.clientId);
-
-            const content = blocks
-                .slice(0, currentBlockIndex)
-                .map((block) => block.attributes.content || '')
-                .join('\n');
-
-            return content;
-        }, []);
-
-        useEffect(() => {
-            if (!attributes.quote && contentBefore) {
-                setIsLoading(true);
-
-                const prompt = "As a social content expert creator, extract the best quote from the provided text. 'Best quote' means the one more likely to drive readers to want to read the content if they were to see the quote on Twitter. Limit the quote to 200 characters maximum";
-                const input = {
-                    prompt: prompt,
-                    context: contentBefore,
-                };
-
-                fetch(pluginDirUrl + 'proxy.php', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(input),
-                })
-                    .then((response) => response.json())
-                    .then((data) => {
-                        const quote = data.choices[0].text.trim();
-                        setAttributes({ quote: quote });
-                        setIsLoading(false);
-                    })
-                    .catch((error) => {
-                        console.error('Error:', error);
-                        setIsLoading(false);
-                    });
-            }
-        }, [contentBefore]);
-
-        const tweetQuote = () => {
-            const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(attributes.quote)}`;
-            window.open(url, '_blank');
-        };
-
-        return (
-            <div className={props.className}>
-                <RichText.Content tagName="blockquote" value={attributes.quote} />
-                {isLoading ? (
-                    <p>Loading...</p>
-                ) : (
-                    <Button isPrimary onClick={tweetQuote}>
-                        Tweet this
-                    </Button>
-                )}
-            </div>
-        );
-    },
-
-    save: (props) => {
-        return (
-            <div>
-                <RichText.Content tagName="blockquote" value={props.attributes.quote} />
-            </div>
-        );
-    },
+  apiVersion: 2,
+  title: __('Social Quote', 'wp-gpt'),
+  icon: 'twitter',
+  category: 'widgets',
+  supports: {
+    html: false,
+  },
+  edit: EditSocialQuote,
+save: () => {
+  return createElement(
+    'div',
+    { className: 'wp-block-wp-gpt-social-quote' },
+    createElement(InnerBlocks.Content)
+  );
+},
 });
