4df725aad2a9a037b0bd1a2eb6870d3c260a2f69
Paolo Belcastro <p3ob7o@users.noreply.github.com>
2023-09-09 13:18:46 +0200

    Update helper-functions.php

--- wp-content/plugins/night-day/inc/helper-functions.php
+++ wp-content/plugins/night-day/inc/helper-functions.php
@@ -1,6 +1,6 @@
 <?php
-function night-day_register_styles() {
+function night_day_register_styles() {
     wp_register_style( 'style1', plugins_url( 'assets/css/style1.css', dirname( __FILE__ ) ) );
     wp_register_style( 'style2', plugins_url( 'assets/css/style2.css', dirname( __FILE__ ) ) );
 }
-add_action( 'wp_enqueue_scripts', 'night-day_register_styles' );
+add_action( 'wp_enqueue_scripts', 'night_day_register_styles' );
